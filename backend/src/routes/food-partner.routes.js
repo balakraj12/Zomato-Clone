fd5ce3ba78bab88c9b8b83dadbbfb303a0398866
balakraj <balakraj46@gmail.com>
2026-01-30 16:36:57 +0530

    this is my check

--- backend/src/routes/food-partner.routes.js
+++ backend/src/routes/food-partner.routes.js
@@ -1,8 +1,12 @@
 
 
 const express = require('express');
-const foodPartnerController = require("../controllers/food-partner.controller");
-const authMiddleware = require("../middlewares/auth.middleware");
+const foodPartnerController = require("../controllers/food-partner.controller"
+
+);
+const authMiddleware = require("../middlewares/auth.middleware"
+    
+);
 
 const router = express.Router();
 
