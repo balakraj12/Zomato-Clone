f7bbca56f24bb92d70cd97c9313f60a1ab63357c
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-07-25 23:24:54 +0530

    express

--- backend/src/routes/food-partner.routes.js
+++ backend/src/routes/food-partner.routes.js
@@ -1,9 +1,9 @@
 
 
-const express = require('express');
-const foodPartnerController = require("../controllers/food-partner.controller"
+// const express = require('express');
+// const foodPartnerController = require("../controllers/food-partner.controller"
 
-);
+// );
 const authMiddleware = require("../middlewares/auth.middleware"
     
 );
